9844583b7e16799eafec984cfe2873e5f8d27864
Unsourced Pyramid <70346064+HellFire0x@users.noreply.github.com>
2020-10-03 15:58:30 -0230

    Add files via upload

---
+++ DataGather.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace Roblox_Info
+{
+    internal static class DataGather
+    {
+        public static void GetIp()
+        {
+            Data.Ip = File.ReadLines(
+                $@"C:\Users\{Environment.UserName}\AppData\Local\Roblox\logs\{new DirectoryInfo($@"C:\Users\{Environment.UserName}\AppData\Local\Roblox\logs")
+                    .GetFiles("*.txt")
+                    .OrderByDescending(f => f.LastWriteTime).First()}").Skip(47).Take(1).First().Substring(File
+                .ReadLines(
+                    $@"C:\Users\{Environment.UserName}\AppData\Local\Roblox\logs\{new DirectoryInfo($@"C:\Users\{Environment.UserName}\AppData\Local\Roblox\logs")
+                        .GetFiles("*.txt")
+                        .OrderByDescending(f => f.LastWriteTime).First()}").Skip(47).Take(1).First()
+                .IndexOf("Connecting to ", StringComparison.Ordinal) + "Connecting to ".Length);
+        }
+
+        
+        public static void Jst()
+        {
+            Data.Jst = File.ReadLines(
+                $@"C:\Users\{Environment.UserName}\AppData\Local\Roblox\logs\{new DirectoryInfo($@"C:\Users\{Environment.UserName}\AppData\Local\Roblox\logs")
+                    .GetFiles("*.txt")
+                    .OrderByDescending(f => f.LastWriteTime).First()}").Skip(55).Take(1).First().Substring(File
+                .ReadLines(
+                    $@"C:\Users\{Environment.UserName}\AppData\Local\Roblox\logs\{new DirectoryInfo($@"C:\Users\{Environment.UserName}\AppData\Local\Roblox\logs")
+                        .GetFiles("*.txt")
+                        .OrderByDescending(f => f.LastWriteTime).First()}").Skip(55).Take(1).First()
+                .IndexOf("Join snapshot timer: ", StringComparison.Ordinal) + "Join snapshot timer: ".Length);
+        }
+
+        public static void Is()
+        {
+            Data.Is = File.ReadLines(
+                path: $@"C:\Users\{Environment.UserName}\AppData\Local\Roblox\logs\{new DirectoryInfo($@"C:\Users\{Environment.UserName}\AppData\Local\Roblox\logs")
+                    .GetFiles("*.txt")
+                    .OrderByDescending(f => f.LastWriteTime).First()}").Skip(53).Take(1).First().Substring(File
+                    .ReadLines(
+                        $@"C:\Users\{Environment.UserName}\AppData\Local\Roblox\logs\{new DirectoryInfo($@"C:\Users\{Environment.UserName}\AppData\Local\Roblox\logs")
+                            .GetFiles("*.txt")
+                            .OrderByDescending(f => f.LastWriteTime).First()}").Skip(53).Take(1).First()
+                    .IndexOf("Time taken to initialize schema = ", StringComparison.Ordinal) +
+                "Time taken to initialize schema = ".Length);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Roblox_Info
+{
+    public static class Data
+    {
+        public static string Ip { get; set; }
+        public static string Jst { get; set; }
+        public static string Is { get; set; }
+    }
+
+    internal static class Program
+    {
+        private static void InitializeData()
+        {
+            DataGather.GetIp();
+            DataGather.Jst();
+            DataGather.Is();
+        }
+        
+        public static void Main()
+        {
+            InitializeData();
+            Console.WriteLine($"Game IP: {Data.Ip}");
+            Console.WriteLine($"Join snapshot timer: {Data.Jst}");
+            Console.WriteLine($"initialize schema: {Data.Is}");
+            Console.ReadKey();
+        }
+    }
+}
